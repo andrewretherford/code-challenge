fa37198aab87c958312cecacc366c12f24e835f7
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-09-27 17:58:45 -0700

    Adds plusone problem

---
+++ plus-one/solution.js
@@ -0,0 +1,19 @@
+// Given a large integer as an array of digits
+// Each element in the array is the nth digit of the integer
+// Digits are arranged from left to right from most significant to least significant
+// Increment the large integer by one and return the resulting array
+
+// Constraints
+/* ----------------------------------
+1 <= digits.length <= 100
+0 <= digits[i] <= 9
+digits does not contain any leading 0's.
+---------------------------------- */
+
+function plusOne(digits) {
+   // Loop backwards through the array from the last element 
+
+   // On each pass, if the current element is less than 9, add 1 and return the array. If it is 9, set it to 0.
+
+   
+}
