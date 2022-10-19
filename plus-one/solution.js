5221d7edca5b8c78c8bf15ab1ad6eebe1bdf2ca9
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-10-19 11:55:13 -0700

    Adds solution for plus-one

--- plus-one/solution.js
+++ plus-one/solution.js
@@ -14,6 +14,15 @@ function plusOne(digits) {
    // Loop backwards through the array from the last element 
 
    // On each pass, if the current element is less than 9, add 1 and return the array. If it is 9, set it to 0.
+   // If the loop arrives at index 0 and the element is 9, un-shift 1 into the array and return.
 
-   
+   for(let i = digits.length-1; i>=0; i--) {
+      if(digits[i] < 9) {
+         digits[i]++
+         return digits
+      } else digits[i] = 0
+   }
+
+   digits.unshift(1)
+   return digits
 }
